3e5b20cf3ac0a2515f08a82f3fd36b31ec21bbde
Rasool Shaik <rsl.basha@gmail.com>
2020-01-10 12:59:34 +0530

    Other commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Is it working?
+          Yes it is
         </p>
         <a
           className="App-link"
